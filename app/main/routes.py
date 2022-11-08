d7a31069689b9d0b436eac59f122896aa3245974
Faheem <117659269+Fah88m@users.noreply.github.com>
2022-11-08 14:52:44 +0000

    Headers In

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 from app.models import User
-
+from flask_login import current_user
 main = Blueprint('main', __name__)
 
 @main.route('/')
 @main.route('/index', methods=['GET', 'POST'])
 def index(cat=None):
-    return render_template('index.html',  title='Home')
+    if current_user.is_authenticated:
+        return render_template('dashboard.html', title='Dashboard')
+    else:
+        return render_template('index.html',  title='Home')
+
+@main.route('/pricing')
+def pricing():
+    return render_template('pricing.html', title="Pricing")
+@main.route('/help')
+def help():
+    return render_template('help.html', title="Help")
