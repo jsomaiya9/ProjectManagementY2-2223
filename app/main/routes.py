972d4eb88f3d13ba7768976728ad33a907000065
bade221 <badealaketu@gmail.com>
2022-12-02 14:29:30 +0000

    Bade Branch

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,26 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 from app.models import User
+from flask_login import current_user
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 @main.route('/index', methods=['GET', 'POST'])
 def index(cat=None):
-    return render_template('index.html',  title='Home')
+    if current_user.is_authenticated:
+      return render_template('dashboard.html',  title='dashboard')
+    else:
+      return render_template('index.html',  title='Home')
+
+@main.route('/pricing')
+def pricing():
+      return render_template('pricing.html',  title='pricing')
+      
+
+@main.route('/aboutus')
+def aboutus():
+      return render_template('aboutus.html',  title='aboutus')
+
+@main.route('/products')
+def products():
+      return render_template('products.html',  title='products')
