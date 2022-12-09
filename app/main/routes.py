23fc3f7cf99cb46ad40c7bcee2ce80806f4a19f7
Tommasz14 <czuczwara@icloud.com>
2022-12-09 15:26:40 +0000

    Tomasz Branch

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,3 +7,39 @@ main = Blueprint('main', __name__)
 @main.route('/index', methods=['GET', 'POST'])
 def index(cat=None):
     return render_template('index.html',  title='Home')
+
+@main.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html',  title='About Us')
+
+@main.route('/account')
+def account():
+    return render_template('account.html',  title=' Account')
+
+@main.route('/contact')
+def contact():
+    return render_template('contact.html',  title='Contact')
+
+@main.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html',  title='Dashboard')
+
+@main.route('/help')
+def help():
+    return render_template('help.html',  title='Help')
+
+@main.route('/prices')
+def prices():
+    return render_template('prices.html',  title='Prices')
+
+@main.route('/pricing')
+def pricing():
+    return render_template('pricing.html',  title='Pricing')
+
+@main.route('/products')
+def products():
+    return render_template('products.html',  title='Products')
+
+@main.route('/newsannouncement')
+def newsannouncement():
+    return render_template('newsannoucement.html',  title='newsannouncement')
