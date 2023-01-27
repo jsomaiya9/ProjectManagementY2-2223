99a700705373eabb438610ec4761addd36a9eaea
Faheem <117659269+Fah88m@users.noreply.github.com>
2023-01-27 15:22:07 +0000

    All done

--- app/main/routes.py
+++ app/main/routes.py
@@ -7,3 +7,33 @@ main = Blueprint('main', __name__)
 @main.route('/index', methods=['GET', 'POST'])
 def index(cat=None):
     return render_template('index.html',  title='Home')
+
+
+@main.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html',  title='About Us')
+
+
+@main.route('/pricing')
+def pricing():
+    return render_template('pricing.html',  title='Pricing')
+
+@main.route('/help')
+def help():
+    return render_template('help.html',  title='Help')
+
+@main.route('/account')
+def account():
+    return render_template('account.html',  title='Account')
+
+@main.route('/contact')
+def contact():
+    return render_template('contact.html',  title='Contact')
+
+@main.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html',  title='Dashboard')
+
+@main.route('/products')
+def products():
+    return render_template('products.html',  title='Products')
